127c381a847ba23b2fdd7f17766581e89c053b3e
Dommo <dominic@formulathoughts.com>
2024-02-09 20:38:07 +0000

    Improve dockerfile with aliases and git config

---
+++ dev-env/setup_bash.sh
@@ -0,0 +1,12 @@
+echo "alias xpmain='poetry run python src/main.py --config config.json'" >> /root/.bashrc
+echo "alias xpapp='poetry run python src/app.py'" >> /root/.bashrc
+echo "alias xprx='poetry run python '" >> /root/.bashrc
+echo "alias xpt='poetry run pytest .'" >> /root/.bashrc
+echo "alias xrf='poetry run ruff format .'" >> /root/.bashrc
+echo "alias ll='ls -l'" >> /root/.bashrc
+
+# Set Git user name
+git config --global user.name "Dommo"
+
+# Set Git user email
+git config --global user.email "dominic@formulathoughts.com"
